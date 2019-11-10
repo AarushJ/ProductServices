96c2940c1912261ca9bb96d7920294eff95bb4c9
AarushJ <avijuneja.cs@gmail.com>
2019-11-10 14:32:44 +0530

    added Implementation of business layer of Product

---
+++ src/main/java/com/maven/udemy/course/bo/ProductBOImpl.java
@@ -0,0 +1,20 @@
+package com.maven.udemy.course.bo;
+
+import com.maven.udemy.course.dao.ProductDAO;
+import com.maven.udemy.course.dto.Product;
+
+public class ProductBOImpl implements ProductBO {
+
+	private ProductDAO dao;
+	
+	@Override
+	public void createProduct(Product product) {
+		dao.create(product);
+	}
+
+	@Override
+	public Product findProduct(int id) {
+		return dao.read(id);
+	}
+
+}
